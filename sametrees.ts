5935a9a70c7392657e07b78c7f336268e71d335d
Taofeek Kassim <taophyckassim@gmail.com>
2025-07-10 17:20:00 +0100

    Add files via upload

---
+++ sametrees.ts
@@ -0,0 +1,8 @@
+// LeetCode #100
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  if (!p && !q) return true;
+  if (!p || !q) return false;
+  if (p.val != q.val) return false;
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
